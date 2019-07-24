70d2479887b2ca515e9969e6c2d8164f80b9a762
basic-app <dev@basic-app.com>
2019-07-24 22:15:56 +0300

    n/a

--- Helpers/app_view_helper.php
+++ Helpers/app_view_helper.php
@@ -1,20 +1,13 @@
 <?php
 /**
  * @package Basic App System
+ * @link http://basic-app.com
  * @license MIT License
- * @link    http://basic-app.com
  */
 if (!function_exists('app_view'))
 {
     function app_view(string $name, array $data = [], array $options = [])
     {
-        $filename = APPPATH . 'Views/' . str_replace('\\', '/', $name) . '.php';
-
-        if (is_file($filename))
-        {
-            $name = 'App\\' . $name;
-        }
-
-        return view($name, $data, $options);
+        return BasicApp\Helpers\ViewHelper::render($name, $data, $options);
     }
 }
