9a46efecf1844a7e2b18d675ad229803a6dcb263
basic-app <dev@basic-app.com>
2019-10-19 17:31:41 +0300

    n/a

--- Config/BaseSystem.php
+++ Config/BaseSystem.php
@@ -8,7 +8,7 @@ namespace BasicApp\System\Config;
 
 use BasicApp\System\Forms\SystemConfigForm;
 
-abstract class BaseSystem extends \BasicApp\Config\DatabaseConfig
+abstract class BaseSystem extends \BasicApp\Config\BaseConfig
 {
 
     protected $modelClass = SystemConfigForm::class;
--- Forms/BaseSystemConfigForm.php
+++ Forms/BaseSystemConfigForm.php
@@ -10,7 +10,7 @@ use BasicApp\System\Config\System as SystemConfig;
 use BasicApp\Core\Form;
 use BasicApp\System\SystemEvents;
 
-abstract class BaseSystemConfigForm extends \BasicApp\Config\DatabaseConfigForm
+abstract class BaseSystemConfigForm extends \BasicApp\Config\BaseConfigForm
 {
 
     protected $returnType = SystemConfig::class;
