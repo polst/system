75bff766f31fa0d3904cc72040d9706758b2259c
basic-app <dev@basic-app.com>
2020-01-21 18:29:23 +0300

    n/a

--- Config/Events.php
+++ Config/Events.php
@@ -1,10 +1,6 @@
 <?php
 
 use BasicApp\System\SystemEvents;
-use BasicApp\Admin\AdminEvents;
-use BasicApp\Site\SiteEvents;
-use BasicApp\Helpers\Url;
-use BasicApp\System\Forms\SystemConfigForm;
 
 SystemEvents::onPreSystem(function()
 {
@@ -17,15 +13,3 @@ SystemEvents::onValidation(function($event)
     $event->ruleSets[] = BasicApp\Validators\NotHtmlTagsValidator::class;
     $event->ruleSets[] = BasicApp\Validators\NotSpecialCharsValidator::class;
 });
-
-if (class_exists(SiteEvents::class))
-{
-    SiteEvents::onSeed(function($created) {
-        if ($created)
-        {
-            block('layout.siteName', 'My Site');
-            block('layout.copyright', '&copy; My Company {year}');
-            block('layout.defaultTitle', 'My Site Default Title');
-        }
-    });
-}
